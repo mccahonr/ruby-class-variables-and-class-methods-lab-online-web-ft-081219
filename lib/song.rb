8592fb0fdbead560801efd61d798492b21a117a8
Ryan McCahon <mccahonr@yahoo.com>
2019-08-21 17:47:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count +=1
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.artist_count
+    count_artists = {}
+    @@artists.each do |artist|
+      if count_artists[artist]
+        count_artists[artist] +=1
+      else
+        count_artists[artist] = 1
+      end
+    end
+    count_artists
+  end
+  def self.genre_count
+    count_genres = {}
+    @@genres.each do |genre|
+      if count_genres[genre]
+        count_genres[genre] += 1
+      else
+        count_genres[genre] = 1
+      end
+    end
+    count_genres
+  end
+end
