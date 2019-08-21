a8f9ebe3416e6ae723a652d1617dc803c423bbbf
Ryan McCahon <mccahonr@yahoo.com>
2019-08-21 17:34:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,7 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @@artist << artist
     @genre = genre
-    @@genre << genre
     @@count +=1
   end
   def method_name
